bff7aae7dbcd2cedc071357e558a3a1c53b19ca5
rajAditya-singh <indrmy.126@gmail.com>
2025-10-09 20:07:16 +0530

    v

--- JavaScript/operator.js
+++ JavaScript/operator.js
@@ -101,7 +101,7 @@ let m3=80
 let eligibilty = m1>75 || m2>90 || m3>70
 // console.log(eligibilty)
 let avg = (m1 + m2 + m3)/3
-console.log(avg)
+// console.log(avg)
 // !(NOT)
 let rightToVote = true
 // console.log(!rightToVote) 
@@ -111,3 +111,90 @@ let rightToVote = true
 
 // console.log(NaN || 0 || false || undefined || null) /* Last Falsy and First Truthy */
 // console.log(NaN || 0 || "String" || false || undefined || null) /* Last Falsy and First Truthy */
+
+
+// /** Type Coercion */
+// console.log('5' + 5) /* String Concatenation */
+// console.log('5' - 5) /* Arithmetic Operation */
+// console.log('5' * 5) /* Arithmetic Operation */
+// console.log('5' / 5) /* Arithmetic Operation */
+// console.log('5' - '2') /* Arithmetic Operation */
+// console.log('5' * '2') /* Arithmetic Operation */
+// console.log('5' / '2') /* Arithmetic Operation */
+// console.log('5' + '2') /* String Concatenation */
+// console.log('5' + true) /* String Concatenation */
+// console.log('5' - true) /* Arithmetic Operation */
+// console.log('5' + false) /* String Concatenation */
+// console.log('5' - false) /* Arithmetic Operation */
+
+ 
+
+/* Type Conversion */
+// convert to Number 
+// console.log("*****To Number*****")
+// console.log(Number('5')) /* String to Number */
+// console.log(Number(true)) /* Boolean to Number */
+// console.log(Number(false)) /* Boolean to Number */
+// console.log(Number(null)) /* Null to Number */
+// console.log(Number(undefined))  /* Undefined to Number */
+// console.log(Number('Hello')) /* Invalid String to Number */ 
+// console.log(Number(NaN)) /* NaN to Number */    
+
+
+// // convert to String
+// console.log("*****To String*****")
+// console.log(String(5)) /* Number to String */
+// console.log(String(true)) /* Boolean to String */
+// console.log(String(false)) /* Boolean to String */
+// console.log(String(null)) /* Null to String */
+// console.log(String(undefined))  /* Undefined to String */
+// console.log(String(NaN)) /* NaN to String */        
+
+// //parseInt
+// console.log("*****parseInt*****")
+// console.log(parseInt('1235f'))
+// console.log(parseInt('f1235f'))
+// console.log(parseInt('12.35f'))
+
+// //parseFloat
+// console.log("*****parseFloat*****")
+// console.log(parseFloat('12.35f'))
+// console.log(parseFloat('f12.35f'))
+
+// // Convert to Boolean
+// console.log("*****To Boolean*****")
+// console.log(Boolean(5)) /* Number to Boolean */
+// console.log(Boolean(0))
+// console.log(Boolean(""))
+// console.log(Boolean(" "))
+// console.log(Boolean("Hello")) /* String to Boolean */
+// console.log(Boolean(null)) /* Null to Boolean */
+// console.log(Boolean(undefined))  /* Undefined to Boolean */
+// console.log(Boolean(NaN)) /* NaN to Boolean */
+
+let num1="50"
+let num3="50"
+let num4="50"
+// let avgnum= (Number(num1) + Number(num3) + Number(num4)) / 3
+let avgnum=(parseFloat(num1) + parseFloat(num3) + parseFloat(num4)) / 3
+
+// console.log(avgnum)
+
+let originalPrice = 1000;
+let discountPercent = 15;
+let finalPrice =originalPrice - (originalPrice * discountPercent) / 100;
+
+console.log(finalPrice)
+
+function temperatureInCelcius(temperatureInCelsius) {
+  // Convert the temperature from Celsius to Fahrenheit
+  let fahrenheit;
+    fahrenheit=(temperatureInCelsius * 9/5) + 32
+  // console.log(fahrenheit)
+  // Check if the Fahrenheit value is greater than 86
+  let isHot;
+    isHot=fahrenheit>86
+  return isHot;
+}
+console.log(temperatureInCelcius(30)); // Output: true
+console.log(temperatureInCelcius(20)); // Output: false
