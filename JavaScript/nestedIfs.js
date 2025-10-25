693f2fe18b2189a1c973e1a4c3e00152d9440cda
rajAditya-singh <indrmy.126@gmail.com>
2025-10-25 10:40:13 +0530

    s

---
+++ JavaScript/nestedIfs.js
@@ -0,0 +1,22 @@
+/** Nested If Statement */
+
+// Check Eligibility for driving license
+// Age >= 18 - eligible else not eligible
+// passed the test - eligible can obtain license
+// failed the test - eligible but cannot obtain license
+
+/*
+let age = 19;
+let passedTest = false;
+
+if (age>=18){
+    console.log("Eligible to obtain driving license");
+    if(passedTest){
+        console.log("Congratulations! You can obtain driving license");
+    }else{
+        console.log("Sorry, you cannot obtain driving license");
+    }
+}else{
+    console.log("Not eligible to obtain driving license");
+}
+*/
