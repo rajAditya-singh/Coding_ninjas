a2750795a769d79d6200f58fa77847ae6dce872b
Aditya Raj Singh <167435837+rajAditya-singh@users.noreply.github.com>
2025-11-15 00:59:52 +0530

    s

--- JavaScript/object.js
+++ JavaScript/object.js
@@ -4,7 +4,7 @@ const student = {
     hobby: "cricket",
     100: 'hundred',
     show: function () {
-        console.log('This is the Student details section');
+        // console.log('This is the Student details section');
     }
 };
 
@@ -20,11 +20,39 @@ const student = {
 // console.log(student.age)
 // console.log(student)
 
-const obj = {
-    a: 1,
-    b: 2,
-    c: 3
+// const obj = {
+//     a: 1,
+//     b: 2,
+//     c: 3
+// }
+// for (const key in obj) {
+//     console.log(key)
+// }
+
+// --------------------------------------
+//complete the calculatePrice function
+//Do not alter the starter code
+const goods = {
+    apple: { price: 150, quantity: 2 },
+    banana: { price: 75, quantity: 3 },
+    orange: { price: 125, quantity: 1 },
+};
+function calculatePrice(goods) {
+    // Hint 1: Initialize a variable to store the total price, e.g., let totalPrice = 0;
+    let totalPrice = 0;
+    // Hint 2: Use a for-in loop to iterate over the properties (items) in the goods object
+    // for (let item in goods) { ... }
+    for (let item in goods) {
+        const { price, quantity } = goods[item];
+        totalPrice += price * quantity;
+    }
+    
+    // Hint 3: Use object destructuring to extract the price and quantity of each item
+
+    // Hint 4: Calculate the total price for each item and add it to the totalPrice
+
+    // Hint 5: Return the totalPrice after the loop
+    return totalPrice;
 }
-for (const key in obj) {
-    console.log(key)
-}
+console.log(calculatePrice(goods));
+//output : 650
