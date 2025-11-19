81fde2ae7f45efa413f787e560ec0bb73a3c2deb
Aditya Raj Singh <167435837+rajAditya-singh@users.noreply.github.com>
2025-11-19 23:52:10 +0530

    s

--- JavaScript/scope.js
+++ JavaScript/scope.js
@@ -4,30 +4,42 @@
 //Functional Scope -- by default all var let const are functionsal scope
 //Block Scope -- only let and const are blocked scope i.e. they can not be accessed outside the block{}
 
-var varNum = 10;
-let letNum = 20
-const constNum = 30
+// var varNum = 10;
+// let letNum = 20
+// const constNum = 30
 
-function print() {
-    var varLocal = 40
-    let letLocal = 50
-    const constLocal = 60
+// function print() {
+//     var varLocal = 40
+//     let letLocal = 50
+//     const constLocal = 60
 
-    console.log(varLocal, letLocal, constLocal)
-    console.log(varNum, letNum, constNum)
+//     console.log(varLocal, letLocal, constLocal)
+//     console.log(varNum, letNum, constNum)
+// }
+
+// console.log(varNum, letNum, constNum)
+// print()
+
+// //Block Scope
+// {
+//     var varBlock = 10;
+//     let letBlock = 20
+//     const constBlock = 30
+//     console.log(letBlock)
+//     console.log(constBlock)
+// }
+// console.log(varBlock)
+// console.log(letBlock)
+// console.log(constBlock)
+
+// var a = 10
+// function foo() {
+    // console.log(a)
+//     var a = 20
+// }
+// foo()
+let obj = {
+    name :"adi",
+    age: 24
 }
-
-console.log(varNum, letNum, constNum)
-print()
-
-//Block Scope
-{
-    var varBlock = 10;
-    let letBlock = 20
-    const constBlock = 30
-    console.log(letBlock)
-    console.log(constBlock)
-}
-console.log(varBlock)
-console.log(letBlock)
-console.log(constBlock)
+console.log(obj2.city)
