1cbd691455f48f9e7666c6032687cca54126c6c7
Aditya Raj Singh <167435837+rajAditya-singh@users.noreply.github.com>
2026-01-05 23:51:43 +0530

    s

--- JavaScript/objLiterals.js
+++ JavaScript/objLiterals.js
@@ -1,29 +1,41 @@
 const movie = {
-    Title: "Marvel's The Avengers",
-    Year: 2012,
-    Rated: "PG-13",
-    Gener: ["Action", "Adventure", "Sci-Fi"],
-    Director: "Joss Whedon",
-    cast: {
-        lead: "Robert Downey Jr.",
-        support: "Chris Evans",
-        villain: "Tom Hiddleston"
-    },
-    Actors: ["Robert Downey Jr.", "Chris Evans", "Mark Ruffalo", "Chris Hemsworth"],
-    BoxOffice: "$623,357,910",
-    getDetails: function () {
-        console.log(
-            `${this.Title}\nYear: ${this.Year},\nDirected by: ${this.Director}.\nStarring: ${this.Actors.join(", ")}.\nBox Office: ${this.BoxOffice}.`
-        );
-    },
+  Title: "Marvel's The Avengers",
+  Year: 2012,
+  Rated: "PG-13",
+  Gener: ["Action", "Adventure", "Sci-Fi"],
+  Director: "Joss Whedon",
+  cast: {
+    lead: "Robert Downey Jr.",
+    support: "Chris Evans",
+    villain: "Tom Hiddleston",
+  },
 
-    getMovieDetails: function (details) {
-        console.log(movie[details])
-    }
-}
+  Actors: [
+    "Robert Downey Jr.",
+    "Chris Evans",
+    "Mark Ruffalo",
+    "Chris Hemsworth"
+  ],
+
+  BoxOffice: "$623,357,910",
+  
+  getDetails() {
+    console.log(
+      `${movie.Title}
+      Year: ${movie.Year},
+      Directed by: ${movie.Director},
+      Starring: ${movie.Actors},
+      Box Office: ${movie.BoxOffice}.`
+    );
+  },
+
+  getMovieDetails(details) {
+    console.log(movie[details]);
+  },
+};
 movie.getDetails();
 
-const details = 'Title';
+const details = "Title";
 movie.getMovieDetails(details);
 
-console.log(movie.cast.villain)
+console.log(movie.cast.villain);
