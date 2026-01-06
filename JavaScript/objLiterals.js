652c9dba198e3d5289c355fcae12d911aa44abd5
rajAditya-singh <indrmy.126@gmail.com>
2026-01-06 15:02:48 +0530

    s

--- JavaScript/objLiterals.js
+++ JavaScript/objLiterals.js
@@ -1,4 +1,4 @@
-const movie = {
+const movie1 = {
   Title: "Marvel's The Avengers",
   Year: 2012,
   Rated: "PG-13",
@@ -21,21 +21,59 @@ const movie = {
   
   getDetails() {
     console.log(
-      `${movie.Title}
-      Year: ${movie.Year},
-      Directed by: ${movie.Director},
-      Starring: ${movie.Actors},
-      Box Office: ${movie.BoxOffice}.`
+      `${this.Title}
+      Year: ${this.Year},
+      Directed by: ${this.Director},
+      Starring: ${this.Actors},
+      Box Office: ${this.BoxOffice}.`
     );
   },
 
-  getMovieDetails(details) {
-    console.log(movie[details]);
-  },
+  // getMovieDetails(details) {
+  //   console.log(movie1[details]);
+  // },
 };
-movie.getDetails();
+movie1.getDetails();
 
-const details = "Title";
-movie.getMovieDetails(details);
+// const details = "Title";
+// movie1.getMovieDetails(details);
 
-console.log(movie.cast.villain);
+// console.log(movie1.cast.villain);
+
+
+const movie2 = {
+  Title: "Movie2",
+  Year: 2020,
+  Rated: "PG-13",
+  Gener: ["Action", "Adventure", "Sci-Fi"],
+  Director: "Joss Whedon",
+  cast: {
+    lead: "Robert Downey Jr.",
+    support: "Chris Evans",
+    villain: "Tom Hiddleston",
+  },
+
+  Actors: [
+    "Robert Downey Jr.",
+    "Chris Evans",
+    "Mark Ruffalo",
+    "Chris Hemsworth"
+  ],
+
+  BoxOffice: "$623,357,910",
+  
+  getDetails() {
+    console.log(
+      `${this.Title}
+      Year: ${this.Year},
+      Directed by: ${this.Director},
+      Starring: ${this.Actors},
+      Box Office: ${this.BoxOffice}.`
+    );
+  },
+
+  // getMovieDetails(details) {
+  //   console.log(movie[details]);
+  // },
+};
+movie2.getDetails();
