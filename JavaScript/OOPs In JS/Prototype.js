e764797c3bb7cf284b1078a82f71afbb39dbc667
rajAditya-singh <indrmy.126@gmail.com>
2026-01-09 18:08:03 +0530

    s

--- JavaScript/OOPs In JS/Prototype.js
+++ JavaScript/OOPs In JS/Prototype.js
@@ -4,12 +4,21 @@ function Movie(title){
 
 const movie1=new Movie("Avatar")
 
-console.log(movie1)
+// console.log(movie1)
 movie1.year=2020
-console.log(movie1)
+// console.log(movie1)
 
 const movie2 = new Movie("Inception")
-console.log(movie2)
+// console.log(movie2)
 
-console.log(movie1.__proto__)
-console.log(movie2.__proto__.__proto__)
+// console.log(movie1.__proto__)
+console.log(movie2.__proto__.__proto__.__proto__)
+
+
+function Game(name, platform){
+    this.name= name;
+    this.platform= platform;
+}
+console.log(Game.prototype)
+const fifa = new Game("Fifa 23", "PS5")
+console.log(fifa.__proto__ === Game.prototype)
