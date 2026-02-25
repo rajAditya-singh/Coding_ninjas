6c06f176809387d093eb4b1b1a51d08634987b4f
Aditya Raj Singh <167435837+rajAditya-singh@users.noreply.github.com>
2026-02-25 23:28:24 +0530

    s

--- JavaScript/ES6 Classes/Class.js
+++ JavaScript/ES6 Classes/Class.js
@@ -21,22 +21,26 @@ class VehicleClass {
   name;
   color;
   wheels;
-
+  //Private Property
+  #regNumber;
   //constructor
-  constructor(name, color, wheels) {
+  constructor(name, color, wheels, number) {
     this.name = name;
     this.color = color;
     this.wheels = wheels;
+    this.#regNumber = number;
   }
 
-  //methods
-  getDetails() { 
+  //methods this is called getter method
+  getDetails() {
     console.log(
-      `name:${this.name}, color:${this.color}, wheels:${this.wheels}`,
+      `name:${this.name}, color:${this.color}, wheels:${this.wheels}, regNumber:${this.#regNumber}`,
     );
   }
 }
-const veh2 = new VehicleClass("Bike", "blue", 2);
+const veh2 = new VehicleClass("Bike", "blue", 2, "ABC123");
 console.log(veh2);
 veh2.getDetails();
+console.log(veh2.name); 
+console.log(veh2.#regNumber); // This will throw an error because #regNumber is private
 console.log(veh2.__proto__.__proto__);
