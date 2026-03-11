2ed89f8fb288248b7576661c89d46f82452f19ad
Aditya Raj Singh <167435837+rajAditya-singh@users.noreply.github.com>
2026-03-11 23:35:56 +0530

    s

--- JavaScript/ES6 Classes/getter_setter.js
+++ JavaScript/ES6 Classes/getter_setter.js
@@ -3,23 +3,35 @@ class Circle {
     this.radius = radius;
   }
 
-//   CalDiameter() {
-//     return this.radius * 2;
-//   }
+  //   CalDiameter() {
+  //     return this.radius * 2;
+  //   }
 
-//Getter
+  //Getter
   get Diameter() {
     return this.radius * 2;
   }
 
   //setter
   set Diameter(newDiameter) {
-    
+    this.radius = newDiameter / 2;
   }
 }
 const circle = new Circle(5);
 console.log(circle.radius);
 // console.log(circle.CalDiameter());
-Circle.Diameter=20; 
+console.log(circle.Diameter);
+circle.radius = 6;
+console.log(circle.radius);
+
+circle.Diameter = 30;
 console.log(circle.Diameter);
 console.log(circle.radius);
+
+
+// Getters and setters are used to:
+
+// ✔ Access private properties
+// ✔ Modify private properties safely
+// ✔ Validate data
+// ✔ Protect internal data
