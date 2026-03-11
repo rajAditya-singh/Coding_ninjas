66f01f5a88e48174fad15ef5576fab84932f195c
rajAditya-singh <indrmy.126@gmail.com>
2026-03-11 11:38:51 +0530

    d

--- JavaScript/ES6 Classes/getter_setter.js
+++ JavaScript/ES6 Classes/getter_setter.js
@@ -11,7 +11,15 @@ class Circle {
   get Diameter() {
     return this.radius * 2;
   }
+
+  //setter
+  set Diameter(newDiameter) {
+    
+  }
 }
 const circle = new Circle(5);
 console.log(circle.radius);
 // console.log(circle.CalDiameter());
+Circle.Diameter=20; 
+console.log(circle.Diameter);
+console.log(circle.radius);
