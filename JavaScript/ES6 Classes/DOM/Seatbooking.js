e51ffc32c5503060e973077fc62020c13d7cb835
rajAditya-singh <indrmy.126@gmail.com>
2026-04-16 18:10:43 +0530

    s

--- JavaScript/ES6 Classes/DOM/Seatbooking.js
+++ JavaScript/ES6 Classes/DOM/Seatbooking.js
@@ -28,6 +28,7 @@ dropDown.addEventListener("change", (e) => {
 
 const seats = document.querySelectorAll("#seatCont .seat");
 // console.log(seats);
+let selectedSeats = [];
 seats.forEach((seat, i) => {
   seat.addEventListener("click", function () {
     if (!seat.classList.contains("occupied")) {
@@ -35,21 +36,52 @@ seats.forEach((seat, i) => {
       seat.style.backgroundColor = seat.classList.contains("selected")
         ? "lightgreen"
         : "";
-      document.getElementById("numberOfSeat").textContent =
-        document.querySelectorAll(".seat.selected").length - 1;
-      let totalPrice =
-        (document.querySelectorAll(".seat.selected").length - 1) *
-        parseFloat(price.textContent.slice(2));
-      document.getElementById("totalPrice").textContent = "$ " + totalPrice;
 
-      let selectedSeateHolder = document.getElementById("selectedSeatsHolder");
-      selectedSeateHolder.textContent = [];
-      document.querySelectorAll(".seat.selected").forEach((seat) => {
-        selectedSeateHolder.textContent = i;
-      });
+      const noOfSeats = document.getElementById("numberOfSeat")
+      noOfSeats.textContent =document.querySelectorAll(".seat.selected").length - 1;
+
+      const seatid = i+1;
+      if (seat.classList.contains("selected")) {
+        selectedSeats.push(seatid);
+      } else {
+        selectedSeats = selectedSeats.filter((id) => id !== seatid);
+      }
+
+      let seatHolder = document.getElementById("selectedSeatsHolder");
+      seatHolder.innerHTML = "";
+      selectedSeats.forEach((id)=>{
+        const takenSeat = document.createElement("div")
+        takenSeat.classList.add("selectedSeat")
+        takenSeat.innerHTML=id;
+        seatHolder.appendChild(takenSeat)
+      })
+
+      let totalPrice = noOfSeats.textContent * parseFloat(price.textContent.slice(2));
+      document.getElementById("totalPrice").textContent = "$ " + totalPrice;
     }
   });
 });
+let proceedBtn = document.getElementById("proceedBtn");
+proceedBtn.addEventListener("click", function () {
+  if(selectedSeats.length > 0){
+    alert("Yayy! Your Seats have been booked.");
+  }else{
+    alert("Oops no seat Selected.")
+  }
+});
+
+let cancelBtn = document.getElementById("cancelBtn");
+cancelBtn.addEventListener("click", function(){
+  document.querySelectorAll(".seat.selected").forEach((seat)=>{
+    seat.classList.remove("selected")
+    seat.classList.add("occupied")
+    seat.style.backgroundColor = "#bdbdbd";
+    document.getElementById("numberOfSeat").textContent= 0
+    document.getElementById("totalPrice").textContent = "$ " + 0;
+    let noSelected=document.querySelector(".noSelected")
+    document.getElementById("selectedSeatsHolder").innerHTML = noSelected.innerHTML;
+  })
+})
 //Add eventLister to each unoccupied seat
 //Add eventLsiter to continue Button
 //Add eventListerner to Cancel Button
