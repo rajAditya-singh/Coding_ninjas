1bcbe73b8044d67cacc4cec2e7b96aaa84975141
Aditya Raj Singh <167435837+rajAditya-singh@users.noreply.github.com>
2026-05-11 23:42:36 +0530

    s

--- JavaScript/ES6 Classes/JS Projects/quiz game app/quiz.js
+++ JavaScript/ES6 Classes/JS Projects/quiz game app/quiz.js
@@ -1,37 +1,87 @@
-const questionObj = {
-  category: "Food & Drink",
-  id: "qa-1",
-  correctAnswer: "Three",
-  options: ["Two", "Three", "Four", "Five"],
-  question: "How many pieces of bun are in a Mcdonald's Big Mac?",
-};
+// const questionObj = {
+//   category: "Food & Drink",
+//   id: "qa-1",
+//   correctAnswer: "Three",
+//   options: ["Two", "Three", "Four", "Five"],
+//   question: "How many pieces of bun are in a Mcdonald's Big Mac?",
+// };
+
+const quesJSON = [
+  {
+    correctAnswer: "Three ",
+    options: ["Two", "Three ", "Four", "Five"],
+    question: "How many pieces of bun are in a Mcdonald's Big Mac?",
+  },
+  {
+    correctAnswer: "L. Frank Baum",
+    options: [
+      "Suzanne Collins",
+      "James Fenimore Cooper",
+      "L. Frank Baum",
+      "Donna Leon",
+    ],
+    question: "Which author wrote 'The Wonderful Wizard of Oz'?",
+  },
+  {
+    correctAnswer: "Atlanta United",
+    options: [
+      "Atlanta United",
+      "Atlanta Impact",
+      "Atlanta Bulls",
+      "Atlanta Stars",
+    ],
+    question: "Which of these is a soccer team based in Atlanta?",
+  },
+  {
+    correctAnswer: "A Nanny",
+    options: ["A Sow", "A Lioness", "A Hen", "A Nanny"],
+    question: "A female goat is known as what?",
+  },
+  {
+    correctAnswer: "P. L. Travers",
+    options: [
+      "J. R. R. Tolkien",
+      "P. L. Travers",
+      "Lewis Carroll",
+      "Enid Blyton",
+    ],
+    question: "Which author wrote 'Mary Poppins'?",
+  },
+];
+
 let score = 0;
-const { correctAnswer, options, question } = questionObj;
+let currentQuestion = 0;
 
-const displayQuestion = document.getElementById("question");
-displayQuestion.textContent = question;
+showQuestions();
 
-const displayOptions = document.getElementById("options");
+function showQuestions() {
+  const { correctAnswer, options, question } = quesJSON[currentQuestion];
 
-const displayScore = document.getElementById("score");
+  const displayQuestion = document.getElementById("question");
+  displayQuestion.textContent = question;
 
-suffleOpt(options).forEach((opt) => {
-  const optionBtn = document.createElement("button");
-  optionBtn.textContent = opt;
-  displayOptions.appendChild(optionBtn);
+  const displayOptions = document.getElementById("options");
 
-  optionBtn.addEventListener("click", () => {
-    if (opt === correctAnswer) {
-      score++;
-    } else {
-      score = score - 0.25;
-    }
-    // console.log(score);
-    displayScore.textContent = `Score : ${score}`;
-    displayQuestion.textContent = "Quiz Completed!";
-    displayOptions.textContent = "";
+  const displayScore = document.getElementById("score");
+
+  suffleOpt(options).forEach((opt) => {
+    const optionBtn = document.createElement("button");
+    optionBtn.textContent = opt;
+    displayOptions.appendChild(optionBtn);
+
+    optionBtn.addEventListener("click", () => {
+      if (opt === correctAnswer) {
+        score++;
+      } else {
+        score = score - 0.25;
+      }
+      // console.log(score);
+      displayScore.textContent = `Score : ${score}`;
+      // displayQuestion.textContent = "Quiz Completed!";
+      // displayOptions.textContent = "";
+    });
   });
-});
+}
 
 function suffleOpt(opt) {
   for (let i = options.length - 1; i >= 0; i--) {
