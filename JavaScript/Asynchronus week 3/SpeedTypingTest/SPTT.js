4c848ef156b066a44809f5566fbe1f6622f59d93
rajAditya-singh <indrmy.126@gmail.com>
2026-06-09 18:53:26 +0530

    s

---
+++ JavaScript/Asynchronus week 3/SpeedTypingTest/SPTT.js
@@ -0,0 +1,10 @@
+const sentences = `The quick brown fox jumps over the lazy dog . Sphinx of black quartz, judge my vow . Pack my box with five dozen liquor jugs . How vexingly quick daft zebras jump !`;
+
+const sentenceElement = document.getElementById("sentence");
+const inputElement = document.getElementById("input");
+const startButton = document.getElementById("start-btn");
+const timerElement = document.getElementById("timer");
+const speedElement = document.getElementById("speed");
+const accuracyElement = document.getElementById("accuracy");
+const resultElement = document.getElementById("result");
+const retryButton = document.getElementById("retry-btn");
