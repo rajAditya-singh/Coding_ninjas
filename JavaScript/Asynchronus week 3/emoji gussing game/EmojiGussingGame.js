61eefb24da12a51569cac63a910626f0e510a7df
rajAditya-singh <indrmy.126@gmail.com>
2026-06-09 17:32:58 +0530

    ss

--- JavaScript/Asynchronus week 3/emoji gussing game/EmojiGussingGame.js
+++ JavaScript/Asynchronus week 3/emoji gussing game/EmojiGussingGame.js
@@ -9,17 +9,22 @@ const emojiDetails = [
 
 let current_index = 0
 let current_score = 0
-
-emojiDisplay()
-// guess_input.addEventListener("keypress", emojiDisplay)
-function emojiDisplay() {
-    const description = document.getElementById("description")
-    description.textContent = emojiDetails[current_index].emoji;
-}
-
+let seconds=10
 const guess_input = document.getElementById("guess-input")
 const scoreEle = document.getElementById("score")
 const resultEle = document.getElementById("result")
+const timerElement = document.getElementById("timer")
+
+
+// guess_input.addEventListener("keypress", emojiDisplay)
+function emojiDisplay() {
+    const description = document.getElementById("description")
+    timerElement.textContent = `Time: ${seconds}s`
+    description.textContent = emojiDetails[current_index].emoji;
+
+    restart_button.style.display="none"
+}
+
 
 function checkGuess(){
     let guess = guess_input.value.trim().toLowerCase();
@@ -38,6 +43,10 @@ function checkGuess(){
 
 function nextEmoji(){
     current_index++
+    setTimeout(()=>{
+        resultEle.textContent = "";
+    }, 2000)
+
 
     if(current_index === emojiDetails.length){
         current_index=0;
@@ -47,10 +56,43 @@ function nextEmoji(){
     emojiDisplay()
 }
 
+function setTimer(){
+    timer = setInterval(()=>{
+        seconds--;
+        timerElement.textContent = `Time: ${seconds}s`
+
+        if(seconds<=0){
+            endGame()
+        }
+    },1000) 
+}
+
+function endGame(){
+    clearInterval(timer)
+    guess_input.disabled=true;
+    timerElement.textContent = "Game Ended"
+
+    restart_button.style.display="block"
+}
+
 document.getElementById("guess-input").addEventListener("keydown", (event)=>{
     if(event.key === "Enter"){
         checkGuess()
     }
 });
 
-document.addEventListener("DOMContentLoaded", emojiDisplay)
+document.addEventListener("DOMContentLoaded", ()=>{
+    emojiDisplay();
+    setTimer()
+})
+
+const restart_button = document.getElementById("restart-button")
+
+restart_button.addEventListener("click", ()=>{
+    current_index = 0
+    current_score = 0
+    seconds=10
+    guess_input.disabled=false;
+    emojiDisplay()
+    setTimer()
+})
