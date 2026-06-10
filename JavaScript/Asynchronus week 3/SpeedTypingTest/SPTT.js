39eac5ec1d586278f05b7bc0f2a324c780502bd3
Aditya Raj Singh <167435837+rajAditya-singh@users.noreply.github.com>
2026-06-11 00:07:48 +0530

    s

--- JavaScript/Asynchronus week 3/SpeedTypingTest/SPTT.js
+++ JavaScript/Asynchronus week 3/SpeedTypingTest/SPTT.js
@@ -9,4 +9,40 @@ const accuracyElement = document.getElementById("accuracy");
 const resultElement = document.getElementById("result");
 const retryButton = document.getElementById("retry-btn");
 
-//
+let seconds = 11
+
+startButton.addEventListener("click", startTest)
+
+function startTest() {
+    inputElement.disabled = false
+    sentenceElement.textContent = sentences
+    startButton.disabled = true
+    startTimer()
+}
+
+function startTimer() {
+    const timer = setInterval(() => {
+        seconds--;
+
+        timerElement.textContent = `00:${seconds.toString().padStart(2, '0')}`
+
+        if (seconds <= 0) {
+            clearInterval(timer)
+
+            resultElement.style.display = "block";
+            inputElement.disabled = true
+            startButton.disabled = true
+        }
+    }, 1000)
+}
+
+retryButton.addEventListener("click", () => {
+    startButton.disabled = false
+    resultElement.style.display = "none";
+    inputElement.value = ""
+    inputElement.disabled = false
+    seconds = 30
+    timerElement.textContent = `00:${seconds.toString().padStart(2, '0')}`
+    startTimer()
+
+})
