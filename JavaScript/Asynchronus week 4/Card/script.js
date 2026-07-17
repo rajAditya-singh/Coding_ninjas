1e53a1005b08d98d48a43f5ef30f18ef3930a528
rajAditya-singh <indrmy.126@gmail.com>
2026-07-17 18:34:17 +0530

    s

--- JavaScript/Asynchronus week 4/Card/script.js
+++ JavaScript/Asynchronus week 4/Card/script.js
@@ -3,20 +3,50 @@ const divEle = document.querySelector(".card-container");
 
 function getUser(id) {
   fetch(`https://dummyjson.com/users/${id}`)
-    // console.log(request)
     .then((response) => {
       if (!response.ok) {
         throw new Error("Something Went Wrong")
       }
-      // console.log(response.headers)
       return response.json();
     })
     .then((user) => {
-      console.log(user)
+      displayUser(user, "beforeend")
+      return fetch(`https://dummyjson.com/users/${id-1}`)
+    })
+    .then((response) => {
+      if (!response.ok) {
+        throw new Error("Cannot load previous Element")
+      }
+      return response.json();
+    })
+    .then((user) => {
+      displayUser(user, 'afterbegin', 'other')
+      return fetch(`https://dummyjson.com/users/${id+1}`)
+    })
+    .then((response) => {
+      if (!response.ok) {
+        throw new Error("Cannot load previous Element")
+      }
+      return response.json();
+    })
+    .then((user) => {
+      displayUser(user, 'beforeend', 'other')
     }).catch((err) => {
-      console.error(err)
       console.log(err)
     })
 }
 
-getUser(0);
+function displayUser(data, position, className='') {
+  const card = `<div class="user-card ${className}">
+      <img src=${data.image} alt="Profile Image" id="dpImage" />
+      <h3 id="firstName">${data.firstName}</h3>
+      <h3 id="lastName">${data.lastName}</h3>
+      <p class="email">${data.email}</p>
+      <button class="btn">View Profile</button>
+      </div>`;
+
+  // divEle.innerHTML = card
+  divEle.insertAdjacentHTML(position, card);
+}
+
+getUser(2);
