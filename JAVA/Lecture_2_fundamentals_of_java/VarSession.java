b688d881230e842d0743942b7343ba1b1416ab9b
Aditya Raj Singh <167435837+rajAditya-singh@users.noreply.github.com>
2026-09-24 00:27:32 +0530

    s

---
+++ JAVA/Lecture_2_fundamentals_of_java/VarSession.java
@@ -0,0 +1,9 @@
+package JAVA.Lecture_2_fundamentals_of_java;
+
+public class VarSession {
+    public static void main(String[] args) {
+        System.out.println(1 + 2);
+        int x = 20;
+        System.out.println(x);
+    }
+}
